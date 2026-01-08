3559ad419064d8ad5200f477be092b36a95b1587
Azhar Iqbal <niazi7589@gmail.com>
2026-01-08 21:31:19 +0500

    chapter 04 Math expressions: Familiar operators

---
+++ chapter04/main.py
@@ -0,0 +1,11 @@
+#11 Assign a number to a variable, and another number to a second variable. Multiply the two, using the variables. Assign the result to a third variable. Display the result using the third variable.
+num1 = 8
+num2=44*2
+result=num1*num2
+print(result)
+
+#12 In a single line of code, divide one number by another and display the result.
+# x=30;
+# print(x/3)
+
+print(144/12)
