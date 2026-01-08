71232830b86f18926804317661b756dd2e93032d
Azhar Iqbal <niazi7589@gmail.com>
2026-01-08 20:17:13 +0500

    chapter01 print

---
+++ chapter01/main.py
@@ -0,0 +1,31 @@
+
+
+
+#01 question. What keyword tells Python to display something on the screen?
+# Answer. print
+#02 question. Complete the line of code to tell Python to display the name of the oval object laid by a hen. (1 word, lower-case)
+# Answer. print("egg")
+#03 question. In the orange box type the next character. I'll autocomplete. Don't type spaces or carriage returns.
+# Tell Python to display the letter "e".
+# Answer. print("e")
+#04 question. Tell Python to display the name of our planet. (1 word, lower-case)
+# Answer. print("earth")
+#05 question.Tell Python to display the name of the star that makes life possible on earth. (1 word, lower-case)
+# Answer. print("sun")
+#06 question.In a print statement, the characters surrounded by quotation marks are a text _______.
+# Answer. string
+#07 question. Tell Python to display the first three letters of the alphabet in upper-case.
+# Answer. print("ABC")
+#08 question. Tell Python to display a string of your choice
+# Answer. print("My Name is Azhar iqbal")
+#09 question. Tell Python to display your first name.
+# Answer. print("My FirstName is Azhar")
+#10 question. Tell Python to display the three-letter word for a canine. (1 word, lower-case)
+# Answer. print("dog")
+#11 question. Python to display the string "Hello, World!"
+# Answer. print("Hello World!")
+print("Hello World!")
+#12 question. Python to display your First Name
+# Answer. print("Azhar")
+print("Azhar")
+
