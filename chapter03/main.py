cb651fb067d39bf256b8da260e5624265e3a5bf2
Azhar Iqbal <niazi7589@gmail.com>
2026-01-08 21:12:34 +0500

    chapter03 variable for number

---
+++ chapter03/main.py
@@ -0,0 +1,33 @@
+# 01 There are two correct answers for this one. Answer with either one. What is this? 100
+# Answer integer or number
+# 02 Type the illegal character in this variable name.  2nd_runner_up  
+# Answer 2
+# 03 Assign the number 5 to the variable players
+# Answer players=5
+# 04 Assign the negative number -6 to the variable loss
+# Answer loss=-6
+# 05 What is the value of total?
+# number = 10
+# total = number + 2
+# Answer 12
+# 06 Assign a number (your choice) to the variable total
+# Answer total = 150
+# 07 Assign a negative integer to a variable. You choose the integer and the variable name.
+#  Answer  total = -150
+# 08 What is this (one word, lowercase)? "-8.087"
+#  Answer string
+# 09 Add two variables and assign the result to a third variable. Make up all the variable names.
+#  Answer 
+# num1 = 10
+# num2 = 20 
+# result=num1+num2
+# print(result)
+#10 assign the sum of two numbers to a variable. Make up the variable name and the numbers. On line 2, tell Python to display the sum using the variable
+num1 = 10
+num2 = 20 
+result=num1+num2
+print(result)
+# 11 I've assigned a number to a variable. On line 2, divide the variable by a number of your choice. Assign the result to a variable of your choice. On line 3, tell Python to display the result using that second variable.
+number = 102
+number1 = number / 5 
+print(number1)
