b777b05e61652f23c78ccfb2d45887b79622a0a1
Azhar Iqbal <niazi7589@gmail.com>
2026-01-08 20:50:04 +0500

    chapter 02 string

---
+++ chapter02/main.py
@@ -0,0 +1,24 @@
+#01 question. Because of the quotation marks, Python identifies the greeting below as a _______ (1 word, lowercase)"Hello, World!"
+# Answer  string
+#02 question Because there are no quotation marks, Python identifies the set of characters below as a _______.country_of_origin
+# Answer variable
+#03 question Assign the string "Boo!" to the variable scare
+# Answer scare="Boo!"
+#04 question Assign the string "jay" to the variable bird.
+# Answer bird="jay"
+#05 question Assign your first name to a variable. You choose the variable name.
+# Answer my_name="Azhar"
+# 06 In the statement below, I assign a string to a variable. Using the variable, write the statement that displays the string on the screen.
+# my_name = "Mark"
+# Answer print(my_name)
+#07  Assign a string to a variable. You choose the variable name and the string. Using the variable, write a statement that displays the string on the screen.
+#Answer city="karachi"
+# print(city)
+#08 question Assign the string "Hi" to the variable greeting
+# Answer greeting="Hi"
+# 09 question choose to create a variable 
+college="Superior Group of college Mianwali"
+print(college)
+# 10 question choose to create a First name 
+First_name="Azhar iqbal"
+print(First_name)
