d87fb9699a8f495070dc59e0fbdb0269048bd42f
Azhar Iqbal <niazi7589@gmail.com>
2026-01-09 14:50:39 +0500

    6 Math expressions: Unfamiliar operators

---
+++ chapter06/main.py
@@ -0,0 +1,15 @@
+# chapter 06 Math expressions: Unfamiliar operators
+# 11 question In a single line of code, display the remainder—that is, what's left over—when 100 is divided by 11.
+number=100%11
+print(number)
+
+
+# 12 question x = 100
+# x = x + 3
+# x = x / x
+# print(x)
+# write shorthand form
+x = 100
+x += 3
+x /= x
+print(x)
