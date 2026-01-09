92d17a0b6dff12387ca9202f0a288108532ee16b
Azhar Iqbal <niazi7589@gmail.com>
2026-01-09 20:10:24 +0500

    10 chapter  Comparison operators

---
+++ chapter10/main.py
@@ -0,0 +1,8 @@
+# 10 chapter  Comparison operators 
+
+#11 question If 1 plus 1 doesn't equal 3, display ok
+if(1+1!=3):
+    print("ok")
+#12 question If 2 is less than or equal to 2, display ok
+if(2<=2):
+    print("ok")
