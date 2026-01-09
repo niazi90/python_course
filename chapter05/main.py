eec9195de7ea433c4f53de56f39a94161b0dfebc
Azhar Iqbal <niazi7589@gmail.com>
2026-01-09 14:40:24 +0500

    chapter05 Variable Names Legal and Illegal

---
+++ chapter05/main.py
@@ -0,0 +1,23 @@
+# chapter05 Variable Names Legal and Illegal
+# 11 question correct this
+#  4h="yes"
+# negative answer = "no"
+# asian_country = "China"
+# african-country = "Kenya"
+# print("ok")
+h4="yes"
+negative_answer = "no"
+asian_country = "China" 
+african_country = "Kenya"
+print(h4, negative_answer, asian_country, african_country)
+
+
+# 12 question correct this
+# 1stvariable = 1
+# another-variable = 2
+# onemorevariable = 3
+# print(another-variable)
+first_variable = 1
+another_variable = 2
+onemore_variable = 3
+print(another_variable)
