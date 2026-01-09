4045f56a0d146e26fceea1bd6fefd74240358164
Azhar Iqbal <niazi7589@gmail.com>
2026-01-09 22:19:09 +0500

    chapter 18  Lists: Deleting and removing elements

---
+++ chapter18/main.py
@@ -0,0 +1,8 @@
+# chapter 18  Lists: Deleting and removing elements
+myylist = ["A","B","C","D"]
+print("using remove")
+myylist.remove("C")
+print(myylist)
+print("using del")
+del myylist[1]
+print(myylist)
