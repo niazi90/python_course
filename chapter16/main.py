183934745efc1c98fbdea4f11976477d712f8cb1
Azhar Iqbal <niazi7589@gmail.com>
2026-01-09 22:10:37 +0500

    chapter 16 Lists: Adding and changing elements

---
+++ chapter16/main.py
@@ -0,0 +1,7 @@
+# chapter 16 Lists: Adding and changing elements
+mylist = ["apple", "banana", "cherry"]
+ulist=mylist.append("orange")
+print(mylist)
+myylist = ["A","B", "C"]
+uulist=myylist.insert(1,"D")
+print(myylist)
