bdc603174bec304d14abee243093add9c86f27ad
Azhar Iqbal <niazi7589@gmail.com>
2026-01-09 15:00:06 +0500

    chapter 7 Math expressions: Eliminating ambiguity

---
+++ chapter07/main.py
@@ -0,0 +1,7 @@
+# chapter 7 Math expressions: Eliminating ambiguity
+# 11 question print(4 * 5 + 10)
+# add parentheses to make it 60
+print(4 * (5 + 10))
+
+# print(5 + 3 / 1 + 1)  Remove ambiguity from this code so the result is 4.
+print((5 + 3) / (1 + 1))
