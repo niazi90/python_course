4a47fc50f1e126e464050a52e27fc86cf974c7d8
Azhar Iqbal <niazi7589@gmail.com>
2026-01-09 15:11:08 +0500

    chapter 08 Concatenating text strings

---
+++ chapter08/main.py
@@ -0,0 +1,13 @@
+# chapter 08 Concatenating text strings
+# 11 question first_word = "Hello"  
+# second_word = "World!"
+# print(first_word + " " + second_word)
+first_word = "Hello"
+second_word = "World!"
+print(first_word + " " + second_word)
+# 12 question Assign three strings to three different variables. Concatenate them and assign the result to a fourth variable. Display the concatenated string using the fourth variable.
+name='Azhar'
+address="karachi"
+country='Pakistan'
+full_details=name +" "+address +" "+country
+print(full_details)
