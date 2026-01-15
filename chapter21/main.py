9136576addeb999c561534ce1bde0c31a9562973
Azhar Iqbal <niazi7589@gmail.com>
2026-01-15 10:36:48 +0500

    chapter 21 for loop and break and continue

---
+++ chapter21/main.py
@@ -0,0 +1,16 @@
+#  chapter 21 for loops
+myylist = ("A","B","C","D")
+# for loop
+for i in myylist:
+    print(i)
+    # using break  
+    print("using break")
+for i in myylist:
+    print(i)
+    break
+# using continue
+print("using continue")
+for i in myylist:
+    print(i)
+    continue
+    
