119852747330e79f16bfe5bec4ca7d20acf16785
Azhar Iqbal <niazi7589@gmail.com>
2026-01-15 11:04:44 +0500

    chapter 23 Getting information from the user and converting strings and numbers

---
+++ chapter23/main.py
@@ -0,0 +1,7 @@
+# chapter 23 Getting information from the user and converting strings and numbers
+
+
+name= input("Enter your name " )
+print("Your name is=> " + str(name))
+age=int(input("Enter your age  "))
+print("your age is =>" + str(age))
