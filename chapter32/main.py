de778727ba096bd61ccf6214cf38aed6eb4406e9
Azhar Iqbal <niazi7589@gmail.com>
2026-02-08 23:52:02 +0500

    chapter 32 Dictionaries: Looping through keys

---
+++ chapter32/main.py
@@ -0,0 +1,14 @@
+# chapter 32 Dictionaries: Looping through keys
+name={
+    "name":"azhar",
+    "age":24,
+    "gender":"male",
+    "adress":"karachi"
+}
+print(name)
+# keys
+print(" keys are:")
+for i in name.values():
+     print(i)
+
+
