bb7315623fd521a21cf1486423cdb0dbe27470d3
Azhar Iqbal <niazi7589@gmail.com>
2026-02-24 00:27:34 +0500

    chapter 35  How to pick information out of a list of dictionaries

---
+++ chapter35/main.py
@@ -0,0 +1,22 @@
+# chapter 35  How to pick information out of a list of dictionaries
+
+name=[
+{
+    "name":"Azhar",
+    "age":24,
+    "gender":"male",
+    "adress":"karachi"
+},{
+    "name":"Abdullah",
+    "age":22,
+    "gender":"male",
+    "adress":"lahore"
+}]
+print(name[0]["name"])
+print(name[0]["age"])
+print(name[0]["adress"])
+print(name[0]["gender"])
+print(name[1]["name"])
+
+
+
