6bffc091e3a8f19729b6ea0a51881742c702ef48
Azhar Iqbal <niazi7589@gmail.com>
2026-02-24 00:13:55 +0500

    chapter 33 Dictionaries: Looping through key value pairs

---
+++ chapter33/main.py
@@ -0,0 +1,16 @@
+# chapter 33 Dictionaries: Looping through key value pairs
+
+
+name={
+    "name":"azhar",
+    "age":24,
+    "gender":"male",
+    "adress":"karachi"
+}
+print(name)
+# keys value pairs
+print(" keys value pairs are:")
+for i in name.items():
+     print(i)
+
+
