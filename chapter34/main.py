c1889480d74208f041bd8068eccd56e185bf8f73
Azhar Iqbal <niazi7589@gmail.com>
2026-02-24 00:19:49 +0500

    chapter 34 Creating a list of dictionaries

---
+++ chapter34/main.py
@@ -0,0 +1,19 @@
+#  chapter 34 Creating a list of dictionaries
+
+
+name=[
+{
+    "name":"Azhar",
+    "age":24,
+    "gender":"male",
+    "adress":"karachi"
+},{
+    "name":"Abdullah",
+    "age":22,
+    "gender":"male",
+    "adress":"lahore"
+}]
+print(name)
+# list of dictionaries
+
+
